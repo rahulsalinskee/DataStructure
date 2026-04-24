544511c66f20af3bb0ff447b88a594e65e86e4fe
rahul <rahulsalinskee@gmail.com>
2026-04-24 18:19:18 +0530

    Data Structure - Queue.

---
+++ DS_Queue/Ds.Queue/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ds.Queue
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        [DataType(DataType.Custom)]
+        public int Age { get; set; }
+
+        public string City { get; set; } = string.Empty;
+    }
+}
---
+++ DS_Queue/Ds.Queue/Operation.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ds.Queue
+{
+    internal class Operation
+    {
+        private Queue<Employee>? _employees = default;
+        private int _id = default;
+
+        public Operation()
+        {
+            _id = 0;
+            _employees = new Queue<Employee>();
+        }
+
+        private int GetTopEmployeeId()
+        {
+            bool isStackEmpty = !_employees.Any();
+
+            try
+            {
+                if (isStackEmpty)
+                {
+                    _id = _employees?.Peek()?.Id ?? 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _id++;
+            }
+            return _id;
+        }
+
+        private (int Id, string Name, int Age, string City) GetNewEmployeeInformationFromUser()
+        {
+            Console.WriteLine("\n\nEnter Employee Details...\n");
+
+            int employeeId = GetTopEmployeeId();
+
+            Console.Write("\nEmployee Name: ");
+
+            /* Read Employee Name From User */
+            var name = Console.ReadLine();
+
+            Console.Write("\nEmployee Age: ");
+
+            /* Read Employee Age From User */
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEmployee City: ");
+
+            /* Read Employee City From User */
+            var city = Console.ReadLine();
+
+            return (employeeId, name, age, city);
+        }
+
+
+        private IEnumerable<Employee> AddEmployee()
+        {
+            var employee = GetNewEmployeeInformationFromUser();
+
+            _employees?.Enqueue(new Employee() { Id = employee.Id, Name = employee.Name, Age = employee.Age, City = employee.City });
+
+            return _employees;
+        }
+
+        private IEnumerable<Employee> RemoveEmployee()
+        {
+            if (!_employees.Any())
+            {
+                Console.WriteLine("No Employee Found!");
+            }
+
+            _employees.Dequeue();
+            return _employees;
+        }
+
+        private void DisplayEmployeesDetail()
+        {
+            if (!_employees.Any())
+            {
+                Console.Write("No Employee Found!");
+                return;
+            }
+            DisplayEmployeeDetails(employees: _employees);
+        }
+
+        private void DisplayTopEmployeeByName()
+        {
+            if (!_employees.Any())
+            {
+                Console.Write("No Employee Found!");
+            }
+
+            Employee topEmployee = _employees.Peek();
+
+            Console.WriteLine("\nTop Employee Name: " + topEmployee.Name);
+            Console.WriteLine();
+        }
+
+        private void GetChoiceDetail()
+        {
+            int choice = default;
+
+            do
+            {
+                Console.WriteLine(" 1. Add Employee \n 2. Get Top Employee \n 3. Get Employee Details \n 4. Remove Employee \n 5. Exit \n");
+                Console.Write("Enter Your Choice: ");
+                var isValidChoice = int.TryParse(Console.ReadLine(), out choice);
+
+                if (isValidChoice)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            var addedEmployees = AddEmployee();
+                            Console.WriteLine($"\nAdded Employee Details...");
+                            Console.WriteLine("--------- --------- ---------\n");
+                            DisplayEmployeeDetails(employees: addedEmployees);
+                            Console.WriteLine("--------- --------- ---------\n");
+                            break;
+
+                        case 2:
+                            DisplayTopEmployeeByName();
+                            break;
+
+                        case 3:
+                            Console.WriteLine();
+                            DisplayEmployeesDetail();
+                            break;
+
+                        case 4:
+                            var remainingEmployee = RemoveEmployee();
+                            Console.WriteLine("\nRemaining Employees After Removing...\n");
+                            DisplayEmployeeDetails(employees: remainingEmployee);
+                            break;
+
+                        case 5:
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice!");
+                }
+            }
+            while (choice != 5);
+        }
+
+        private void DisplayEmployeeDetails(IEnumerable<Employee> employees)
+        {
+            if (!employees.Any())
+            {
+                Console.Write("\n--- No Employee Found! ---");
+            }
+            else
+            {
+                foreach (var employee in employees)
+                {
+                    Console.Write("Id: " + employee.Id + " | Name: " + employee.Name + " | Age: " + employee.Age + " | City: " + employee.City);
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+
+        internal void Run()
+        {
+            GetChoiceDetail();
+        }
+    }
+}
